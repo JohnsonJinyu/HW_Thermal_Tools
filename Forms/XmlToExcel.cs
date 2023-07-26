0b3935bf8110d4361e7628932d258a4ae0e92d5b
鹏程 左 <zuopengcheng00@gmail.com>
2023-07-26 19:50:27 +0800

    neeed  improve

--- Forms/XmlToExcel.cs
+++ Forms/XmlToExcel.cs
@@ -20,6 +20,10 @@ namespace HW_Thermal_Tools.Forms
 {
     public partial class XmlToExcel : Form
     {
+
+        //定义一个全局变量ExcelTitleList，用于保存Excel表格的标题
+        List<string> ExcelTitleList = new List<string>();
+
         public XmlToExcel()
         {
             InitializeComponent();
@@ -89,7 +93,8 @@ namespace HW_Thermal_Tools.Forms
             }
             try
             {
-                ReadFile();
+                //GetExcelTitle();
+                LoadExcelToDic();
 
 
             }
@@ -101,33 +106,117 @@ namespace HW_Thermal_Tools.Forms
 
         }
 
+        
 
-        private void ReadFile()
+        private void GetExcelTitle()
         {
             //读取用户选择的XML文件
             XmlDocument doc = new XmlDocument();
             doc.Load(TxtXmlFilePath.Text);
             /*
-             首先遍历所有的gear_config节点，获取每个节点的属性的名称；
+            1、首先用SelectNodes获取XML文件中所有的gear_config节点；
+            2、gear_config没有子节点，信息保存在他的属性中；
+            3、读取第一个gear_config节点的所有属性名称，保存到ExcelTitleList中；
+            4、继续遍历其他的gear_config节点，读取每个节点的属性；如果有新的属性名称，则添加到ExcelTitleList中；如果没有，则不处理；
              */
-            XmlNodeList nodeList = doc.SelectSingleNode("gear_config").ChildNodes;
-            //遍历所有的gear_config节点
+            XmlNodeList nodeList = doc.SelectNodes("//gear_config");
+            //List<string> ExcelTitleList = new List<string>();
             foreach (XmlNode node in nodeList)
             {
-
+                XmlAttributeCollection attrCol = node.Attributes;
+                foreach (XmlAttribute attr in attrCol)
+                {
+                    if (!ExcelTitleList.Contains(attr.Name))
+                    {
+                        ExcelTitleList.Add(attr.Name);
+                    }
+                }
             }
 
 
+            /*
+            1、读取用户选择的配置表excel文件，遍历每个worksheet页；
+            2、遍历每个sheet页的内容，并将内容保存到一个字典中。
+            3、遍历ExcelTitleList，做以下操作：
+                将“CPU”sheet第一行第二列的内容，插入到ExcelTitleList中“cpu”后面，作为cpu属性值描述的列的标题；
+                将“GPU”sheet第一行第二列的内容，插入到ExcelTitleList中“gpu”后面，作为gpu属性值描述的列的标题；
+                将“亮度等级”sheet第一行第二列的内容，插入到ExcelTitleList中“brightness”后面，作为brightness属性值描述的列的标题；
+                将“充电电流”sheet第一行第二列的内容，插入到ExcelTitleList中“charge_”后面，作为charge属性值描述的列的标题；
+                将“温度档位”sheet第一行第二列和第一行第三列的内容，插入到ExcelTitleList中“tempGear”后面，作为tempGear属性值描述的列的标题；
 
+            */
+           
+            //打印出ExcelTitleList中的内容
+            foreach (string str in ExcelTitleList)
+            {
+                Console.WriteLine(str);
+            }
 
-
-
-
+            //返回ExcelTitleList
+            
 
         }
 
 
 
+
+
+
+
+        /*
+        1、定义一个方法LoadExcelToDic()，用于读取用户选择的配置表excel文件，遍历每个worksheet页；
+        2、从TxtConfigFile.Text中获取用户选择的配置表excel文件路径；
+        3、读取excel文件，将每个sheet页的内容保存到一个字典中；
+        4、每个sheet页第一行为字典的标题，第二行开始：第一列为字典key，第二列开始为字典value；
+        5、每个字典的名称与sheet页的名称相同；
+        6、返回字典；
+        7、设置非商业用途的许可证
+        */
+        private Dictionary<string, Dictionary<string, string>> LoadExcelToDic()
+        {
+            //设置非商业用途的许可证
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            //从TxtConfigFile.Text中获取用户选择的配置表excel文件路径
+            string path = TxtConfigFile.Text;
+            //定义一个字典，用于保存excel文件中每个sheet页的内容
+            Dictionary<string, Dictionary<string, string>> dic = new Dictionary<string, Dictionary<string, string>>();
+            //读取excel文件
+            using (ExcelPackage package = new ExcelPackage(new System.IO.FileInfo(path)))
+            {
+                //遍历每个worksheet页
+                foreach (ExcelWorksheet worksheet in package.Workbook.Worksheets)
+                {
+                    //定义一个字典，用于保存每个sheet页的内容
+                    Dictionary<string, string> sheetDic = new Dictionary<string, string>();
+                    //遍历每个sheet页的内容，并将内容保存到一个字典中
+                    for (int i = 1; i <= worksheet.Dimension.Rows; i++)
+                    {
+                        //第一行为字典的标题
+                        if (i == 1)
+                        {
+                            for (int j = 1; j <= worksheet.Dimension.Columns; j++)
+                            {
+                                sheetDic.Add(worksheet.Cells[i, j].Value.ToString(), "");
+                            }
+                        }
+                        //第二行开始：第一列为字典key，第二列开始为字典value
+                        else
+                        {
+                            for (int j = 1; j <= worksheet.Dimension.Columns; j++)
+                            {
+                                sheetDic[worksheet.Cells[1, j].Value.ToString()] = worksheet.Cells[i, j].Value.ToString();
+                            }
+                        }
+                    }
+                    //每个字典的名称与sheet页的名称相同
+                    dic.Add(worksheet.Name, sheetDic);
+                }
+            }
+            //返回字典
+            return dic;
+        }
+        
+        
     }
-    
+
 }
